3906baa725d60f8b5726231965a897df45d64125
Tarik Amar <tarik.amar@gmail.com>
2023-02-14 13:42:02 +0000

    Navbar enhance design

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,5 +4,7 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
