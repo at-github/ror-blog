df4df89c878b183a0178b40f0e01e69579a7cfac
Tarik Amar <tarik.amar@gmail.com>
2023-02-15 16:10:16 +0000

    New model Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_15_151122) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_160717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_151122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "article_type", default: 0
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "links", force: :cascade do |t|
@@ -40,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_15_151122) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "articles"
 end
